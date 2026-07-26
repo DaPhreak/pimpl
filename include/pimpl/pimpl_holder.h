6532b0a1e2f72727e1b407cb2e2fa8a458a5d8df
Dennis <dennis.kuznik@novaskystories.com>
2026-07-26 15:25:19 +0200

    Some conditionally removed functions (SFINAE)

--- include/pimpl/pimpl_holder.h
+++ include/pimpl/pimpl_holder.h
@@ -20,62 +20,70 @@ public:
     using reference       = T&;
     using const_reference = T const&;
 
+    template <class U = T,typename = std::enable_if_t<std::is_default_constructible_v<U>>>
     pimpl_holder () noexcept(std::is_nothrow_constructible_v<T> && std::is_nothrow_constructible_v<provider_type>)
     {
         new(mProvider.data()) T{};
     }
 
+    //template <class U = T,typename = std::enable_if_t<std::is_copy_constructible_v<U>>> -> needs concepts
     pimpl_holder (pimpl_holder const& S) noexcept(std::is_nothrow_copy_constructible_v<T> && std::is_nothrow_copy_constructible_v<provider_type>)
     : mProvider{S.mProvider}
     {
+        static_assert(std::is_copy_constructible_v<T>,"value_type is not copy constructible");
         new(mProvider.data()) T{*S};
     }
 
+    //template <class U = T,typename = std::enable_if_t<std::is_move_constructible_v<U>>> -> needs concepts
     pimpl_holder (pimpl_holder&& S) noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_move_constructible_v<provider_type>)
     : mProvider{std::move(S.mProvider)}
     {
+        static_assert(std::is_move_constructible_v<T>,"value_type is not move constructible");
         new(mProvider.data()) T{std::move(*S)};
     }
 
-    template<class... Args,std::enable_if_t<std::is_constructible_v<T,Args...>>* = nullptr>
+    template<class... Args,typename = std::enable_if_t<std::is_constructible_v<T,Args...>>>
     explicit pimpl_holder (Args&&... args) noexcept(std::is_nothrow_constructible_v<T,Args...> && std::is_nothrow_constructible_v<provider_type>)
     {
         new(mProvider.data()) T(std::forward<Args>(args)...);
     }
 
-    template<class... Args,std::enable_if_t<std::is_constructible_v<T,Args...>>* = nullptr>
+    template<class... Args,typename = std::enable_if_t<std::is_constructible_v<T,Args...>>>
     explicit pimpl_holder (provider_type const& S,Args&&... args) noexcept(std::is_nothrow_constructible_v<T,Args...> && std::is_nothrow_copy_constructible_v<provider_type>)
     : mProvider{S}
     {
         new(mProvider.data()) T(std::forward<Args>(args)...);
     }
 
-    template<class P,class... Args,std::enable_if_t<std::is_constructible_v<T,Args...>>* = nullptr>
+    template<class P,class... Args,typename = std::enable_if_t<std::is_constructible_v<T,Args...>>>
     explicit pimpl_holder (provider_arg_t,P&& p,Args&&... args) noexcept(std::is_nothrow_constructible_v<T,Args...> && std::is_nothrow_copy_constructible_v<provider_type>)
     : mProvider{std::forward<P>(p)}
     {
         new(mProvider.data()) T(std::forward<Args>(args)...);
     }
 
-
     ~pimpl_holder () noexcept
     {
         (**this).~T();
     }
 
+    //template <class U = T,typename = std::enable_if_t<std::is_copy_assignable_v<U>>> -> needs concepts
     pimpl_holder& operator = (pimpl_holder const& S) noexcept(std::is_nothrow_copy_assignable_v<T>)
     {
+        static_assert(std::is_copy_assignable_v<T>,"value_type is not copy assignable");
         **this=*S;
         return *this;
     }
 
+    //template <class U = T,typename = std::enable_if_t<std::is_move_assignable_v<U>>> -> needs concepts
     pimpl_holder& operator = (pimpl_holder&& S) noexcept(std::is_nothrow_move_assignable_v<T>)
     {
+        static_assert(std::is_move_assignable_v<T>,"value_type is not move assignable");
         **this=std::move(*S);
         return *this;
     }
 
-    template <class U=T,std::enable_if_t<std::is_assignable_v<T,U>>* = nullptr>
+    template <class U = T,typename = std::enable_if_t<std::is_assignable_v<T,U>>>
     pimpl_holder& operator = (U&& S) noexcept(std::is_nothrow_assignable_v<T,U>)
     {
         **this=std::forward<U>(S);
@@ -91,6 +99,11 @@ public:
 
     [[nodiscard]] constexpr T const*  operator -> () const   noexcept { return &(**this); }
     [[nodiscard]] constexpr T*        operator -> ()         noexcept { return &(**this); }
+
+    [[nodiscard]] constexpr operator  T const&    () const&  noexcept { return **this; }
+    [[nodiscard]] constexpr operator  T&          () &       noexcept { return **this; }
+    [[nodiscard]] constexpr operator  T const&&   () const&& noexcept { return std::move(**this); }
+    [[nodiscard]] constexpr operator  T&&         () &&      noexcept { return std::move(**this); }
 
     [[nodiscard]] constexpr T const&  value       () const&  noexcept { return **this; }
     [[nodiscard]] constexpr T&        value       () &       noexcept { return **this; }
